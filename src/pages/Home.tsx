3391dae34cd08662030545fa7065d1c4c973939c
daniel amekpoagbe <amekpoagbedaniel@gmail.com>
2025-06-28 01:05:19 +0000

    added footer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,8 @@
 import { HeroSection } from "@/components/custom/HeroSection";
 import { SkillCard } from "@/components/custom/SkillCard";
-import { Separator } from "@/components/ui/separator";
 import { Navbar } from "@/components/custom/NavBar";
 import HowToGetStarted from "@/components/custom/GetStarted";
-import { Footer } from "@/components/custom/Footer";
+import Footer from "@/components/custom/Footer";
 
 const popularSkills = [
   {
@@ -30,32 +29,12 @@ export const HomePage = () => {
         <HowToGetStarted />
 
         {/* Popular Skills Section */}
-        <section className="space-y-10 text-center">
-          <div className="space-y-4">
-            <h2 className="text-3xl sm:text-4xl md:text-5xl font-bold leading-tight">
-              Popular Skills
-            </h2>
-            <p className="text-gray-400 max-w-2xl mx-auto text-base sm:text-lg">
-              Explore in-demand skills that you can learn or teach within the
-              SwapSkill community.
-            </p>
-          </div>
-
-          <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-3">
-            {popularSkills.map((skill) => (
-              <SkillCard
-                key={skill.id}
-                title={skill.title}
-                description={skill.description}
-              />
-            ))}
-          </div>
-        </section>
       </main>
 
       {/* Footer or Call to Action - Can be added here */}
 
-      <Separator className="border-white/10 mt-20" />
+      <div className="border-t border-white/10 mt-20"></div>
+
       <Footer />
     </div>
   );
