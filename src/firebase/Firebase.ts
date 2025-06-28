e33c847aeeaa7d2ddc9baac1b59527624968f4f6
daniel amekpoagbe <amekpoagbedaniel@gmail.com>
2025-06-28 01:27:05 +0000

    added footer

--- src/firebase/Firebase.ts
+++ src/firebase/Firebase.ts
@@ -3,14 +3,24 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBxJyFiBBz3gal2q4Y6w5njGIiCrGrBEKU",
+//   authDomain: "swapskill-dc959.firebaseapp.com",
+//   projectId: "swapskill-dc959",
+//   storageBucket: "swapskill-dc959.firebasestorage.app",
+//   messagingSenderId: "1041162090524",
+//   appId: "1:1041162090524:web:2bd95ed6905984e96907d9",
+//   measurementId: "G-5T3Q471L6Q",
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBxJyFiBBz3gal2q4Y6w5njGIiCrGrBEKU",
-  authDomain: "swapskill-dc959.firebaseapp.com",
-  projectId: "swapskill-dc959",
-  storageBucket: "swapskill-dc959.firebasestorage.app",
-  messagingSenderId: "1041162090524",
-  appId: "1:1041162090524:web:2bd95ed6905984e96907d9",
-  measurementId: "G-5T3Q471L6Q",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
