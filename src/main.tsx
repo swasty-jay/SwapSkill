12f9294bf41fe8de72002c2027a10bdd916fb022
daniel amekpoagbe <amekpoagbedaniel@gmail.com>
2025-07-02 22:16:34 +0000

    working on the sidebar

--- src/main.tsx
+++ src/main.tsx
@@ -4,22 +4,18 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SidebarProvider } from "./components/ui/sidebar.tsx";
 
-
-
-
-const queryclient= new QueryClient()
-
-
-
+const queryclient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
-
   <QueryClientProvider client={queryclient}>
     <BrowserRouter>
-      <StrictMode>
-        <App />
-      </StrictMode>
+      <SidebarProvider>
+        <StrictMode>
+          <App />
+        </StrictMode>
+      </SidebarProvider>
     </BrowserRouter>
   </QueryClientProvider>
 );
